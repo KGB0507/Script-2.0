e4dff4ee1216e8520e916f31235ca68ebb698d0b
KGB0507 <91737610+KGB0507@users.noreply.github.com>
2023-09-13 17:56:05 +0300

    Add files via upload

--- Script/Parser.py
+++ Script/Parser.py
@@ -4,70 +4,87 @@ import time
 start_time = time.time()
 
 class cryst_coord:
-    def __init__(self, x, y, f1f2f3):
+    def __init__(self, x, y, jumpers):
         self.x = x
         self.y = y
-        self.f1f2f3 = f1f2f3
+        self.jumpers = jumpers
+
+def is_cryst(num_of_column):
+    row = 0
+    while(df.iloc[row, num_of_column] == 0):
+        row += 1
+    if(df.iloc[row, num_of_column] != 0):
+        return True
+    return False
 
 x = 0
 dx = 650
 y = 0
 dy = 470
+num_of_crystals = 0
 
 # Чтение файла Excel
 df = pd.read_excel('Карта перемычек на пластине +цвета_НОРМ.xlsx')
 
-# Номер строки, с которой нужно начать чтение (нулевая координата по x)
-start_row = df[df['Координаты (мкм)'] == 0].index.item()
-
 # Замена пустых значений на символ '-'
 df.fillna('-', inplace=True)
 
-# Номер стартового столбца (нулевая координата по y)
-start_column = df.columns.get_loc(0)
-current_column = start_column
+# Номер строки, с нулевой координатой по x
+nul_row = df[df['Координаты (мкм)'] == 0].index.item()
 
-# Номер последнего столбца, до которого нужно читать данные
-# end_column = start_column + 1
+# Номер столбца с нулевой координатой по y
+nul_column = df.columns.get_loc(0)
 
-# Список для хранения прочитанных столбцов
-# data = []
+# Номер стартового столбца
+current_column = 1
+
 # список для хранения перемычек каждого кристалла
 cryst_coords = []
 
 # Цикл по столбцам
-while (df.iloc[start_row, current_column] != 0):
-    column_data = df.iloc[:(start_row + 1), current_column].tolist()
-    while 0 in column_data:
-        column_data.remove(0)
-    #data.append(column_data)
-    for item in reversed(column_data):
-        if item == '':
-            item = '-'
-        temp_cryst_coord = cryst_coord (x, y, item)
-        y = y + dy
-        cryst_coords.append(temp_cryst_coord)
-    current_column = current_column + 1
-    y = 0
-    x = x + dx
+try:
+    while (current_column < len(df.columns)):
+        # Поиск строки, в которой находится самый нижний кристалл в стартовом столбце
+        #print('while 1')
+        row = 0
+        while (df.iloc[row, current_column] == 0):
+            row += 1
+        last_row = row
 
-# Вывод прочитанных данных
-# for i, column_data in enumerate(data):
-    #print(f"Столбец {i + 1}: {column_data}")
+        #print(last_row + 2)
+        #input()
+
+        #print('while 2')
+        while(df.iloc[row, current_column] != 0 and row < len(df.index) - 1 ):
+            row += 1
+        first_row = row - 1
+        #print(first_row + 2)
+        #input()
+
+        column_data = df.iloc[(last_row - 1):(first_row + 1), current_column].tolist()
+        while 0 in column_data:
+            column_data.remove(0)
+        row = first_row
+        x = (current_column - nul_column) * dx
+        for item in reversed(column_data):
+            y = (nul_row - row) * dy
+            temp_cryst_coord = cryst_coord (x, y, item)
+            cryst_coords.append(temp_cryst_coord)
+            num_of_crystals += 1
+            row -= 1
+        current_column += 1
+        #print(f'end current_column {current_column-1}')
+except:
+    print(f'row - 1 = {row - 1}, current_column = {current_column}, {df.iloc[row-1, current_column]}, {len(df.index)}')
 
 # Открытие файла для записи
-with open('output.txt', 'w') as cryst_coordinates:
+with open('CrystCoordinates.txt', 'w') as cryst_coordinates:
     for item in cryst_coords:
-        cryst_coordinates.write(str(item.x) + '\t' + str(item.y) + '\t' + item.f1f2f3 + '\n')
+        cryst_coordinates.write(str(item.x) + '\t' + str(item.y) + '\t' + item.jumpers + '\n')
 
-# Load the xlsx file
-# excel_data = pd.read_excel('Карта перемычек на пластине +цвета_НОРМ.xlsx')
-# Read the values of the file in the dataframe
-# column = pd.DataFrame(excel_data, columns=["Координаты (мкм)"])
-
-# print("The content of the file is:\n", column)
-# print("The content of the file is:\n", excel_data)pip
-# column[column['Координаты (мкм)']] == '0'.index [0]
+print(f'Number of crystals: {num_of_crystals}')
+print(f'NUMOFCOLS = {len(df.columns)}')
+print(f'WDISTBETWCRYST + WIDTHOFCRYST = {dx}')
 
 end_time = time.time()
 elapsed_time = end_time - start_time
